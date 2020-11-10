2091c6024f9285b6bc244fbb99a2f622ba5d8af1
Sam Lai <supralai@gmail.com>
2020-11-11 00:55:49 +0800

    Add python in place swap version solution for 832

--- 832 - Flipping an Image/README.md
+++ 832 - Flipping an Image/README.md
@@ -1,9 +1,16 @@
 # Result
 
 ## Python
+### Version 1
 ```
 Runtime: 32 ms, faster than 90.40% of Python online submissions for Flipping an Image.
 Memory Usage: 13.5 MB, less than 22.28% of Python online submissions for Flipping an Image.
+```
+
+### Version 2
+```
+Runtime: 24 ms, faster than 99.82% of Python online submissions for Flipping an Image.
+Memory Usage: 13.3 MB, less than 22.28% of Python online submissions for Flipping an Image.
 ```
 
 ## Go
--- 832 - Flipping an Image/solve.py
+++ 832 - Flipping an Image/solve.py
@@ -4,10 +4,21 @@ class Solution(object):
         :type A: List[List[int]]
         :rtype: List[List[int]]
         """
-        result = []
+        # Version 2: in place swap
+        for row in A:
+            l = 0
+            r = len(row)-1
+            while l <= r:
+                row[l], row[r] = row[r] ^ 1, row[l] ^ 1
+                l += 1
+                r -= 1
+        return A
+
+        # Version1
+        """result = []
         for row in A:
             r = []
             for i in range(len(row)-1, -1, -1):
                 r.append(row[i] ^ 1)
             result.append(r)
-        return result
+        return result"""
